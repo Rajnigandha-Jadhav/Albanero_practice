5cc2cfcbac3af556c0421a979719978dd0cd1e88
Rajnigandha-Jadhav <jadhavrajnigandha@gmail.com>
2023-03-31 19:54:38 +0530

    flask

---
+++ datamesh.py
@@ -0,0 +1,18 @@
+# Data Mesh is a data architecture paradigm introduced by Zhamak Dehghani in 2020. It suggests a decentralized approach to data management that treats data as a product and distributes ownership and governance across the organization. Some key terms in the Data Mesh framework include:
+
+# Domain: A domain is a self-contained business area with its own data needs and responsibilities. Domains should have clear boundaries and be able to own and manage their data independently.
+
+# Data Product: A data product is a unit of data that is produced, consumed, and maintained by a domain. It should have clear ownership, documentation, and quality standards, and be treated as a valuable asset.
+
+# Data Mesh Team: A Data Mesh team is a cross-functional group of experts from different domains who collaborate to build and maintain data products. They are responsible for the end-to-end lifecycle of data products.
+
+# Data Ownership: Data ownership is the responsibility and accountability for a data product. In the Data Mesh approach, ownership is distributed to domains, and each domain is responsible for the quality, accuracy, and compliance of their data.
+
+# Data Governance: Data governance is the process of managing and ensuring the quality, availability, usability, and security of data. In the Data Mesh approach, governance is distributed to domains, and each domain is responsible for governing their own data.
+
+# Data Mesh Platform: A Data Mesh platform is a set of tools and infrastructure that support the creation, discovery, consumption, and governance of data products. It provides a standardized way for domains to interact with data products and ensures consistency across the organization.
+
+# Federated Architecture: Federated architecture is an approach to data management that allows multiple systems to work together seamlessly. In the Data Mesh approach, domains are free to choose their own technology stacks, and the Data Mesh platform provides a federated view of all data products.
+
+
+# A data lake can include structured data from relational databases (rows and columns), semi-structured data (CSV, logs, XML, JSON), unstructured data (emails, documents, PDFs), and binary data (images, audio, video).
---
+++ venv/Lib/flask1.py
@@ -0,0 +1,7 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def welcome():
+    return "Welcome To Albanero"
+if __name__ == "__main__":
+    app.run()
